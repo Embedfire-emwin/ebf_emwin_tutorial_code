4f80913cda17c269b5480105f89deb35dcbe83d7
hejiawen <hjw0415@utlook.com>
2019-11-02 15:03:24 +0800

    增加bmp图片显示例程，完成添加代码，需要挂载sdk测试

---
+++ F429_挑战者V1/23. FreeRTOS_STemWIN544_BMP图片显示/User/touch/bsp_i2c_touch.h
@@ -0,0 +1,50 @@
+#ifndef __I2C_TOUCH_H
+#define	__I2C_TOUCH_H
+
+#include "stm32f4xx.h"
+
+#define GTP_ADDRESS            0xBA
+
+#define I2CT_FLAG_TIMEOUT         ((uint32_t)0x1000)
+#define I2CT_LONG_TIMEOUT         ((uint32_t)(10 * I2CT_FLAG_TIMEOUT))
+
+
+#define GTP_I2C                          I2C2
+#define GTP_I2C_CLK                      RCC_APB1Periph_I2C2
+
+#define GTP_I2C_SCL_PIN                  GPIO_Pin_4                  
+#define GTP_I2C_SCL_GPIO_PORT            GPIOH                       
+#define GTP_I2C_SCL_GPIO_CLK             RCC_AHB1Periph_GPIOH
+#define GTP_I2C_SCL_SOURCE               GPIO_PinSource4
+#define GTP_I2C_SCL_AF                   GPIO_AF_I2C2
+
+#define GTP_I2C_SDA_PIN                  GPIO_Pin_5                  
+#define GTP_I2C_SDA_GPIO_PORT            GPIOH                    
+#define GTP_I2C_SDA_GPIO_CLK             RCC_AHB1Periph_GPIOH
+#define GTP_I2C_SDA_SOURCE               GPIO_PinSource5
+#define GTP_I2C_SDA_AF                   GPIO_AF_I2C2
+
+#define GTP_RST_GPIO_PORT                GPIOD
+#define GTP_RST_GPIO_CLK                 RCC_AHB1Periph_GPIOD
+#define GTP_RST_GPIO_PIN                 GPIO_Pin_11
+
+#define GTP_INT_GPIO_PORT                GPIOD
+#define GTP_INT_GPIO_CLK                 RCC_AHB1Periph_GPIOD
+#define GTP_INT_GPIO_PIN                 GPIO_Pin_13
+#define GTP_INT_READ()                   GPIO_ReadInputDataBit(GPIOD,GPIO_Pin_13)
+
+//#define GTP_INT_EXTI_PORTSOURCE          EXTI_PortSourceGPIOD
+//#define GTP_INT_EXTI_PINSOURCE           EXTI_PinSource12
+
+
+static uint32_t I2C_TIMEOUT_UserCallback(void);
+
+
+uint32_t I2C_WriteBytes(uint8_t ClientAddr,uint8_t* pBuffer,  uint8_t NumByteToWrite);
+uint32_t I2C_ReadBytes(uint8_t ClientAddr,uint8_t* pBuffer, uint16_t NumByteToRead);
+
+void I2C_Touch_Init(void);
+void I2C_ResetChip(void);
+
+
+#endif /* __I2C_TOUCH_H */

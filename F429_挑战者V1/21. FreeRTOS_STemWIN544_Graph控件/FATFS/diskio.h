51bc12d3db8a4af2fd5e1b2dcd7635416806b573
hejiawen <hjw0415@utlook.com>
2019-11-04 14:15:49 +0800

    修改fatfs文件系统

--- F429_挑战者V1/21. FreeRTOS_STemWIN544_Graph控件/FATFS/diskio.h
+++ F429_挑战者V1/21. FreeRTOS_STemWIN544_Graph控件/FATFS/diskio.h
@@ -1,32 +1,16 @@
 /*-----------------------------------------------------------------------/
-/  Low level disk interface modlue include file   (C)ChaN, 2014          /
+/  Low level disk interface modlue include file   (C)ChaN, 2013          /
 /-----------------------------------------------------------------------*/
 
 #ifndef _DISKIO_DEFINED
 #define _DISKIO_DEFINED
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-#include "integer.h"	
-	
-#ifndef FATFS_USE_SDIO
-	#define FATFS_USE_SDIO			  1
-#endif
-
-#ifndef FATFS_FLASH_SPI
-	#define FATFS_FLASH_SPI				0
-#endif
-
-#ifndef FATFS_USE_USB
-	#define FATFS_USE_USB			    0
-#endif
-	
 #define _USE_WRITE	1	/* 1: Enable disk_write function */
 #define _USE_IOCTL	1	/* 1: Enable disk_ioctl fucntion */
 
-
+#include "./integer.h"
+//#include "defines.h"
+#include "./attributes.h"
 
 /* Status of Disk Functions */
 typedef BYTE	DSTATUS;
@@ -41,16 +25,17 @@ typedef enum {
 } DRESULT;
 
 
+//#define FATFS_DEBUG_SEND_USART(x)	TM_USART_Puts(USART6, x); TM_USART_Puts(USART6, "\n");
+#define FATFS_DEBUG_SEND_USART(x)
+
 /*---------------------------------------*/
 /* Prototypes for disk control functions */
-
 
 DSTATUS disk_initialize (BYTE pdrv);
 DSTATUS disk_status (BYTE pdrv);
 DRESULT disk_read (BYTE pdrv, BYTE* buff, DWORD sector, UINT count);
 DRESULT disk_write (BYTE pdrv, const BYTE* buff, DWORD sector, UINT count);
 DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
-
 
 /* Disk Status Bits (DSTATUS) */
 
@@ -61,14 +46,14 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
 /* Command code for disk_ioctrl fucntion */
 
-/* Generic command (Used by FatFs) */
-#define CTRL_SYNC			0	/* Complete pending write process (needed at _FS_READONLY == 0) */
-#define GET_SECTOR_COUNT	1	/* Get media size (needed at _USE_MKFS == 1) */
-#define GET_SECTOR_SIZE		2	/* Get sector size (needed at _MAX_SS != _MIN_SS) */
-#define GET_BLOCK_SIZE		3	/* Get erase block size (needed at _USE_MKFS == 1) */
-#define CTRL_TRIM			4	/* Inform device that the data on the block of sectors is no longer used (needed at _USE_TRIM == 1) */
+/* Generic command (used by FatFs) */
+#define CTRL_SYNC			0	/* Flush disk cache (for write functions) */
+#define GET_SECTOR_COUNT	1	/* Get media size (for only f_mkfs()) */
+#define GET_SECTOR_SIZE		2	/* Get sector size (for multiple sector size (_MAX_SS >= 1024)) */
+#define GET_BLOCK_SIZE		3	/* Get erase block size (for only f_mkfs()) */
+#define CTRL_ERASE_SECTOR	4	/* Force erased a block of sectors (for only _USE_ERASE) */
 
-/* Generic command (Not used by FatFs) */
+/* Generic command (not used by FatFs) */
 #define CTRL_POWER			5	/* Get/Set power status */
 #define CTRL_LOCK			6	/* Lock/Unlock media removal */
 #define CTRL_EJECT			7	/* Eject media */
@@ -86,8 +71,11 @@ DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 #define ATA_GET_MODEL		21	/* Get model name */
 #define ATA_GET_SN			22	/* Get serial number */
 
-#ifdef __cplusplus
-}
-#endif
+/* MMC card type flags (MMC_GET_TYPE) */
+#define CT_MMC		0x01		/* MMC ver 3 */
+#define CT_SD1		0x02		/* SD ver 1 */
+#define CT_SD2		0x04		/* SD ver 2 */
+#define CT_SDC		(CT_SD1|CT_SD2)	/* SD */
+#define CT_BLOCK	0x08		/* Block addressing */
 
 #endif

0ce06dac897fcbdf4f78f293c9f8d10dbf4c3bf9
hejiawen <hjw0415@utlook.com>
2019-11-06 08:41:44 +0800

    修改触屏驱动，解决了gt917s触屏乱跳的问题

--- F429_挑战者V1/28. FreeRTOS_STemWIN544_SIF字体显示/User/main.c
+++ F429_挑战者V1/28. FreeRTOS_STemWIN544_SIF字体显示/User/main.c
@@ -324,14 +324,6 @@ static void BSP_Init(void)
 	LCD_Init();
   /* 禁用WIFI模块 */
   BL8782_PDN_INIT();
-  /* 挂载文件系统，挂载时会对SD卡初始化 */
-	/*本例程未使用sd卡*/
-//  result = f_mount(&fs,"0:",1);
-//	if(result != FR_OK)
-//	{
-//		printf("SD卡初始化失败，请确保SD卡已正确接入开发板，或换一张SD卡测试！\n");
-//		while(1);
-//	}
 }
 
 /**

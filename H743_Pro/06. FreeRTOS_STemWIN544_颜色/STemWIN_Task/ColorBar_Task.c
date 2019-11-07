48cff0cbedbc5b15257a9ee60acd03df3a58fdf3
hejiawen <hjw0415@utlook.com>
2019-11-07 11:45:50 +0800

    新增743 pro教程，测试通过

---
+++ H743_Pro/06. FreeRTOS_STemWIN544_颜色/STemWIN_Task/ColorBar_Task.c
@@ -0,0 +1,137 @@
+/**
+  ***********************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   FreeRTOS v9.0.0 + STemWIN 5.44a
+  ***********************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 F429 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ***********************************************************************
+  */
+/*******************************************************************************
+ * 包含的头文件
+ ******************************************************************************/
+#include "ColorBar_Task.h"
+
+/*
+*************************************************************************
+*                                宏定义
+*************************************************************************
+*/
+/* 起始坐标 */
+#define X_START 60
+#define Y_START 40
+
+/*******************************************************************************
+ * 全局变量
+ ******************************************************************************/
+/* */
+typedef struct {
+  int NumBars;
+
+  GUI_COLOR Color;
+  const char * s;
+} BAR_DATA;
+
+static const BAR_DATA _aBarData[] = {
+  { 2, GUI_RED    , "Red" },
+  { 2, GUI_GREEN  , "Green" },
+  { 2, GUI_BLUE   , "Blue" },
+  { 1, GUI_WHITE  , "Grey" },
+  { 2, GUI_YELLOW , "Yellow" },
+  { 2, GUI_CYAN   , "Cyan" },
+  { 2, GUI_MAGENTA, "Magenta" },
+};
+
+static const GUI_COLOR _aColorStart[] = { GUI_BLACK, GUI_WHITE };
+
+/*
+*************************************************************************
+*                                 函数
+*************************************************************************
+*/
+/**
+  * @brief 色条显示函数
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+static void _DemoShowColorBar(void) 
+{
+	GUI_RECT Rect;
+	int      yStep;
+	int      i;
+	int      j;
+	int      xSize;
+	int      ySize;
+	int      NumBars;
+	int      NumColors;
+
+	xSize = LCD_GetXSize();
+	ySize = LCD_GetYSize();
+	
+	/* 可以显示的色条数 */
+	NumColors = GUI_COUNTOF(_aBarData);
+	for (i = NumBars = 0, NumBars = 0; i < NumColors; i++) 
+	{
+		NumBars += _aBarData[i].NumBars;
+	}
+	yStep = (ySize - Y_START) / NumBars;
+	
+	/* 显示文本 */
+	Rect.x0 = 0;
+	Rect.x1 = X_START - 1;
+	Rect.y0 = Y_START;
+	GUI_SetFont(&GUI_Font16B_ASCII);
+	for (i = 0; i < NumColors; i++) 
+	{
+		Rect.y1 = Rect.y0 + yStep * _aBarData[i].NumBars - 1;
+		GUI_DispStringInRect(_aBarData[i].s, &Rect, GUI_TA_LEFT | GUI_TA_VCENTER);
+		Rect.y0 = Rect.y1 + 1;
+	}
+	
+  /* 绘制色条 */
+	Rect.x0 = X_START;
+	Rect.x1 = xSize - 1;
+	Rect.y0 = Y_START;
+	for (i = 0; i < NumColors; i++) 
+	{
+		for (j = 0; j < _aBarData[i].NumBars; j++) 
+		{
+			Rect.y1 = Rect.y0 + yStep - 1;
+			GUI_DrawGradientH(Rect.x0, Rect.y0, Rect.x1, Rect.y1, _aColorStart[j], _aBarData[i].Color);
+			Rect.y0 = Rect.y1 + 1;
+		}
+	}
+}
+
+/**
+  * @brief GUI主任务
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+void MainTask(void)
+{
+  /* 设置背景色 */
+	GUI_SetBkColor(GUI_BLACK);
+	GUI_Clear();
+	
+	/* 设置前景色、字体大小 */
+	GUI_SetColor(GUI_RED);
+	GUI_SetFont(&GUI_Font24B_ASCII);
+	
+	/* 显示色条 */
+	_DemoShowColorBar();
+	
+	while(1) 
+	{
+		GUI_Delay(10);
+	}
+}
---
+++ H743_Pro/07. FreeRTOS_STemWIN544_内存设备/STemWIN_Task/MainTask.c
@@ -0,0 +1,126 @@
+/**
+  *********************************************************************
+  * @file    MainTask.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   FreeRTOS v9.0.0 + STM32 工程模版
+  *********************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 F429 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */
+/*******************************************************************************
+ * 包含的头文件
+ ******************************************************************************/
+
+#include <stdio.h>
+
+#include "GUI.h"
+#include "DIALOG.h"
+#include "MainTask.h"
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*******************************************************************
+*
+*       static code
+*
+********************************************************************
+*/
+/**
+  * @brief 绘图函数
+  * @note 无
+  * @param 
+  * @retval 无
+  */
+static void _Draw(int x0, int y0, int x1, int y1, int i)
+{
+  char buf[] = {0};
+
+  /* 绘制矩形背景 */
+  GUI_SetColor(GUI_BLUE);
+	GUI_FillRect(x0, y0, x1, y1);
+  
+  /* 绘制文本 */
+	GUI_SetFont(GUI_FONT_D64);
+  GUI_SetTextMode(GUI_TEXTMODE_XOR);
+  sprintf(buf, "%d", i);
+	GUI_DispStringHCenterAt(buf, x0 + (x1 - x0)/2, (y0 + (y1 - y0)/2) - 32);
+}
+
+/**
+  * @brief 内存设备演示函数
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+static void _DemoMemDev(void)
+{
+  GUI_MEMDEV_Handle hMem = 0;
+	int i = 0;
+  
+	/* 设置背景色 */
+  GUI_SetBkColor(GUI_BLACK);
+  GUI_Clear();
+  
+	/* 显示提示文字 */
+  GUI_SetColor(GUI_WHITE);
+  GUI_SetFont(GUI_FONT_32_ASCII);
+  GUI_DispStringHCenterAt("MEMDEV_MemDev - Sample", 400, 5);
+  GUI_SetFont(GUI_FONT_24_ASCII);
+  GUI_DispStringHCenterAt("Shows the advantage of using a\nmemorydevice", 400, 45);
+  GUI_SetFont(GUI_FONT_16_ASCII);
+  GUI_DispStringHCenterAt("Draws the number\nwithout a\nmemory device", 100, 90);
+  GUI_DispStringHCenterAt("Draws the number\nusing a\nmemory device", 680, 90);
+  
+  /* 创建内存设备 */
+  hMem = GUI_MEMDEV_Create(600, 150, 150, 100);
+  
+	while (1)
+	{
+    /* 直接绘制 */
+    _Draw(25, 150, 175, 250, i);
+    
+    /* 激活内存设备 */
+    GUI_MEMDEV_Select(hMem);
+    /* 向内存设备中绘制图形 */
+    _Draw(600, 150, 750, 250, i);
+    /* 选择LCD */
+    GUI_MEMDEV_Select(0);
+    /* 将内存设备中的内容复制到LCD */
+    GUI_MEMDEV_CopyToLCDAt(hMem, 600, 150);
+
+		GUI_Delay(40);
+    i++;
+		if (i > 999)
+		{
+			i = 0;
+		}
+	}
+}
+
+/*********************************************************************
+*
+*       Public code
+*
+**********************************************************************
+*/
+/**
+  * @brief GUI主任务
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+void MainTask(void)
+{
+	/* 运行内存设备演示DEMO */
+  _DemoMemDev();
+}
+
+/*************************** End of file ****************************/

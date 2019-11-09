99732b97fabd344a793e413621c1261d5bc6623b
hejiawen <hjw0415@utlook.com>
2019-11-09 17:30:26 +0800

    新增字体显示例程，测试通过

---
+++ F407_霸天虎/28. FreeRTOS_STemWIN544_SIF字体显示/STemWin_Task/text.c
@@ -0,0 +1,4 @@
+﻿const char Framewin_text[] = {"STemWIN@野火电子 STM32F07"};
+const char text[] = {"欢迎使用野火F407开发板\r\n这是一个使用SIF格式字库显示的例子\r\n本例子支持中文显示"};
+const char MULTIEDIT_text[] = {"实验平台:野火 STM32F407 开发板\r\n论坛:http://www.firebbs.cn\r\n淘宝:https://fire-stm32.taobao.com"};
+const char *BUTTON_text[] = {"确定","取消"};

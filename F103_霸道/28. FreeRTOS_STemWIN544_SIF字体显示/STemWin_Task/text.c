7a46f2ee5d99a9fe3fe1e29febae5b51bc71726a
hejiawen <hjw0415@utlook.com>
2019-11-12 09:11:33 +0800

    增加字体显示例程，调整了字号，测试通过

---
+++ F103_霸道/28. FreeRTOS_STemWIN544_SIF字体显示/STemWin_Task/text.c
@@ -0,0 +1,4 @@
+﻿const char Framewin_text[] = {"STemWIN@野火电子 STM32F103"};
+const char text[] = {"欢迎使用野火F103开发板\r\n本例程支持中文\r\n使用SIF格式字库显示\r\n"};
+const char MULTIEDIT_text[] = {"实验平台:野火 STM32F103 开发板\r\n论坛:http://www.firebbs.cn\r\n淘宝:https://fire-stm32.taobao.com"};
+const char *BUTTON_text[] = {"确定","取消"};

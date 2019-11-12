d5e0bd302c7c86abbfb20a998742235f3fdc2690
hejiawen <hjw0415@utlook.com>
2019-11-12 09:25:50 +0800

    修改SDIO优先级分组

--- F103_mini/01. FreeRTOS_STemWIN544_移植模版/User/sdio/bsp_sdio_sdcard.c
+++ F103_mini/01. FreeRTOS_STemWIN544_移植模版/User/sdio/bsp_sdio_sdcard.c
@@ -345,7 +345,7 @@ static void NVIC_Configuration(void)
   NVIC_InitTypeDef NVIC_InitStructure;
 
   /* Configure the NVIC Preemption Priority Bits */
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 
   NVIC_InitStructure.NVIC_IRQChannel = SDIO_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;

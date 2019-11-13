e27157c81055c8d4f7da285fc10faf06b9fb6d2c
hejiawen <hjw0415@utlook.com>
2019-11-13 15:04:16 +0800

    增加xbf字体显示例程，，由于内存限制，取消按钮控件，测试通过

---
+++ F103_指南者/27. FreeRTOS_STemWIN544_XBF字体显示/STemWin_Task/text.c
@@ -0,0 +1,4 @@
+﻿const char Framewin_text[] = {"STemWIN@野火电子STM32F103"};
+const char text[] = {"欢迎使用野火F103开发板\r\n本例程使用XBF格式字库\r\n支持中文显示"};
+const char MULTIEDIT_text[] = {"实验平台:野火 STM32F103 开发板\r\n论坛:\r\nhttp://www.firebbs.cn\r\n淘宝:\r\nhttps://fire-stm32.taobao.com"};
+//const char *BUTTON_text[] = {"确定","取消"};

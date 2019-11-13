9c361504eaed7cc838146081004e36fd9319cf8b
hejiawen <hjw0415@utlook.com>
2019-11-13 10:14:31 +0800

    增加f103指南者例程，测试通过，部分例程由于ROM和RAM的限制，减少了部分演示

---
+++ F103_指南者/16. FreeRTOS_STemWIN544_Dropdown控件/STemWin_Task/DropdownDLG.h
@@ -0,0 +1,9 @@
+#ifndef __DROPDOWNDLG_H
+#define __DROPDOWNDLG_H
+
+#include "GUI.h"
+#include "DIALOG.h"
+
+void MainTask(void);
+
+#endif /* __DROPDOWNDLG_H */
---
+++ F103_指南者/21. FreeRTOS_STemWIN544_Graph控件/STemWin_Task/GraphDLG.h
@@ -0,0 +1,10 @@
+#ifndef __GRAPHDLG_H
+#define __GRAPHDLG_H
+
+#include "GUI.h"
+#include "DIALOG.h"
+#include "GRAPH.h"
+
+void MainTask(void);
+
+#endif /* __GRAPHDLG_H */

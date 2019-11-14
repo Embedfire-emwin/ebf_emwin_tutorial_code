ad8b16edbac512caf009d82df2745c4d63e0fdff
hejiawen <hjw0415@utlook.com>
2019-11-14 16:44:28 +0800

    新增H750例程，测试通过

---
+++ H750_Pro/03. FreeRTOS_STemWIN544_数值显示/User/main.c
@@ -0,0 +1,329 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   电容触摸屏-触摸画板
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32H750开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************
+  */  
+#include "stm32h7xx.h"
+#include "main.h"
+#include "./led/bsp_led.h" 
+#include "./usart/bsp_debug_usart.h"
+#include "./sdram/bsp_sdram.h" 
+#include "./lcd/bsp_lcd.h"
+#include "./touch/bsp_i2c_touch.h"
+#include "./touch/bsp_touch_gtxx.h"
+#include "./delay/core_delay.h" 
+#include "./mpu/bsp_mpu.h" 
+
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+/* STemWIN头文件 */
+#include "GUI.h"
+#include "DIALOG.h"
+
+/**************************** 任务句柄 ********************************/
+/* 
+ * 任务句柄是一个指针，用于指向一个任务，当任务创建好之后，它就具有了一个任务句柄
+ * 以后我们要想操作这个任务都需要通过这个任务句柄，如果是自身的任务操作自己，那么
+ * 这个句柄可以为NULL。
+ */
+/* 创建任务句柄 */
+static TaskHandle_t AppTaskCraete_Handle = NULL;
+/* LED任务句柄 */
+static TaskHandle_t LED_Task_Handle = NULL;
+/* Touch任务句柄 */
+static TaskHandle_t Touch_Task_Handle = NULL;
+/* GUI任务句柄 */
+static TaskHandle_t GUI_Task_Handle = NULL;
+
+/********************************** 内核对象句柄 *********************************/
+/*
+ * 信号量，消息队列，事件标志组，软件定时器这些都属于内核的对象，要想使用这些内核
+ * 对象，必须先创建，创建成功之后会返回一个相应的句柄。实际上就是一个指针，后续我
+ * 们就可以通过这个句柄操作这些内核对象。
+ *
+ * 内核对象说白了就是一种全局的数据结构，通过这些数据结构我们可以实现任务间的通信，
+ * 任务间的事件同步等各种功能。至于这些功能的实现我们是通过调用这些内核对象的函数
+ * 来完成的
+ * 
+ */
+SemaphoreHandle_t ScreenShotSem_Handle = NULL;
+ 
+/******************************* 全局变量声明 ************************************/
+/*
+ * 当我们在写应用程序的时候，可能需要用到一些全局变量。
+ */
+
+
+/*
+*************************************************************************
+*                             函数声明
+*************************************************************************
+*/
+static void AppTaskCreate(void);
+
+static void LED_Task(void* parameter);
+static void Touch_Task(void* parameter);
+static void GUI_Task(void* parameter);
+
+static void BSP_Init(void);
+extern void MainTask(void);
+extern void SpeedTest(void);
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{
+	BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+	
+	/* 开发板硬件初始化 */
+	BSP_Init();
+	
+	xReturn = xTaskCreate((TaskFunction_t)AppTaskCreate,/* 任务入口函数 */
+											 (const char*    )"AppTaskCreate",/* 任务名称 */
+											 (uint16_t       )1024,					/* 任务栈大小 */
+											 (void*          )NULL,					/* 任务入口函数参数 */
+											 (UBaseType_t    )1,						/* 任务的优先级 */
+											 (TaskHandle_t   )&AppTaskCraete_Handle);/* 任务控制块指针 */
+	/* 启动任务调度 */
+	if(pdPASS == xReturn)
+		vTaskStartScheduler();/* 启动任务，开启调度 */
+	else
+		return -1;
+	
+	while(1);/* 正常不会执行到这里 */
+}
+
+/**
+  * @brief 任务创建函数
+  * @note 为了方便管理，所有的任务创建都放在这个函数里面
+  * @param 无
+  * @retval 无
+  */
+static void AppTaskCreate(void)
+{
+	BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+	
+	taskENTER_CRITICAL();//进入临界区
+
+	xReturn = xTaskCreate((TaskFunction_t)LED_Task,/* 任务入口函数 */
+											 (const char*    )"LED_Task",/* 任务名称 */
+											 (uint16_t       )128,       /* 任务栈大小 */
+											 (void*          )NULL,      /* 任务入口函数参数 */
+											 (UBaseType_t    )5,         /* 任务的优先级 */
+											 (TaskHandle_t   )&LED_Task_Handle);/* 任务控制块指针 */
+	if(pdPASS == xReturn)
+		printf("创建LED1_Task任务成功！\r\n");
+	
+	xReturn = xTaskCreate((TaskFunction_t)Touch_Task,/* 任务入口函数 */
+											 (const char*      )"Touch_Task",/* 任务名称 */
+											 (uint16_t         )256,     /* 任务栈大小 */
+											 (void*            )NULL,    /* 任务入口函数参数 */
+											 (UBaseType_t      )4,       /* 任务的优先级 */
+											 (TaskHandle_t     )&Touch_Task_Handle);/* 任务控制块指针 */
+	if(pdPASS == xReturn)
+		printf("创建Touch_Task任务成功！\r\n");
+  
+  xReturn = xTaskCreate((TaskFunction_t)GUI_Task,/* 任务入口函数 */
+											 (const char*      )"GUI_Task",/* 任务名称 */
+											 (uint16_t         )1024 * 4,      /* 任务栈大小 */
+											 (void*            )NULL,      /* 任务入口函数参数 */
+											 (UBaseType_t      )3,         /* 任务的优先级 */
+											 (TaskHandle_t     )&GUI_Task_Handle);/* 任务控制块指针 */
+	if(pdPASS == xReturn)
+		printf("创建GUI_Task任务成功！\r\n");
+	
+	vTaskDelete(AppTaskCraete_Handle);//删除AppTaskCreate任务
+	
+	taskEXIT_CRITICAL();//退出临界区
+}
+
+/**
+  * @brief LED任务主体
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+static void LED_Task(void* parameter)
+{
+	while(1)
+	{
+//    printf("%d\r\n", (int)GUI_ALLOC_GetNumUsedBytes());
+		LED3_TOGGLE;
+		vTaskDelay(1000);
+	}
+}
+
+/**
+  * @brief 触摸检测任务主体
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+static void Touch_Task(void* parameter)
+{
+	while(1)
+	{
+		GT9xx_GetOnePiont();
+		vTaskDelay(20);
+	}
+}
+
+/**
+  * @brief GUI任务主体
+  * @note 无
+  * @param 无
+  * @retval 无
+  */
+static void GUI_Task(void* parameter)
+{
+	/* 初始化GUI */
+	GUI_Init();
+	/* 开启三缓冲 */
+	WM_MULTIBUF_Enable(1);
+
+	while(1)
+	{
+    MainTask();
+//    GUI_Delay(5000);
+	}
+}
+
+/**
+  * @brief 板级外设初始化
+  * @note 所有板子上的初始化均可放在这个函数里面
+  * @param 无
+  * @retval 无
+  */
+static void BSP_Init(void)
+{
+  SCB_EnableICache();
+  SCB_EnableDCache();
+  Board_MPU_Config(0, MPU_Normal_WT, 0x20000000, MPU_REGION_SIZE_128KB);
+  Board_MPU_Config(1, MPU_Normal_WT, 0x24000000, MPU_REGION_SIZE_512KB);
+  /* 如果配置为WT，则在使用Alpha混合时需要手动清空D-Cache，但RGB565下清空操作无效 */
+  Board_MPU_Config(2, MPU_Normal_WT, 0xD0000000, MPU_REGION_SIZE_32MB);
+  
+	/* CRC和emWin没有关系，只是他们为了库的保护而做的
+   * 这样STemWin的库只能用在ST的芯片上面，别的芯片是无法使用的。
+   */
+  __HAL_RCC_CRC_CLK_ENABLE();
+	
+	/*
+	 * STM32中断优先级分组为4，即4bit都用来表示抢占优先级，范围为：0~15
+	 * 优先级分组只需要分组一次即可，以后如果有其他的任务需要用到中断，
+	 * 都统一用同一个优先级分组，千万不要再分组，切记。
+	 */
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	
+	/* 系统时钟初始化成400MHz */
+	SystemClock_Config();
+	/* LED 端口初始化 */
+	LED_GPIO_Config();
+	/* 配置串口1为：115200 8-N-1 */
+	DEBUG_USART_Config();		
+  /* 初始化触摸屏 */
+  GTP_Init_Panel(); 
+  /* 初始化SDRAM 用作LCD 显存*/
+	SDRAM_Init();
+
+	/* LCD 端口初始化 */ 
+	LCD_Init();
+ 
+}
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 400000000 (CPU Clock)
+	*            HCLK(Hz)             = 200000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  100MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  100MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  100MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  100MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 160
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+  
+  /*使能供电配置更新 */
+  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
+
+  /* 当器件的时钟频率低于最大系统频率时，电压调节可以优化功耗，
+		 关于系统频率的电压调节值的更新可以参考产品数据手册。  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+ 
+  /* 启用HSE振荡器并使用HSE作为源激活PLL */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+ 
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+
+/****************************END OF FILE***************************/
---
+++ H750_Pro/19. FreeRTOS_STemWIN544_Slider控件/STemWIN_Task/SliderDLG.h
@@ -0,0 +1,10 @@
+#ifndef __SLIDERDLG_H
+#define __SLIDERDLG_H
+
+#include "GUI.h"
+#include "DIALOG.h"
+#include "SLIDER.h"
+
+void MainTask(void);
+
+#endif /* __SLIDERDLG_H */

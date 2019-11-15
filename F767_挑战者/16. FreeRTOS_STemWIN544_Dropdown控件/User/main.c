7b8a0b745f9de6a5ac1a8a60154a43ff890bc9f4
hejiawen <hjw0415@utlook.com>
2019-11-15 09:27:06 +0800

    增大任务栈，解决了两个下拉栏频繁切换会卡死的问题

--- F767_挑战者/16. FreeRTOS_STemWIN544_Dropdown控件/User/main.c
+++ F767_挑战者/16. FreeRTOS_STemWIN544_Dropdown控件/User/main.c
@@ -139,7 +139,7 @@ static void AppTaskCreate(void)
   
   xReturn = xTaskCreate((TaskFunction_t)GUI_Task,/* 任务入口函数 */
 											 (const char*      )"GUI_Task",/* 任务名称 */
-											 (uint16_t         )1024,      /* 任务栈大小 */
+											 (uint16_t         )1024 * 8,      /* 任务栈大小 */
 											 (void*            )NULL,      /* 任务入口函数参数 */
 											 (UBaseType_t      )3,         /* 任务的优先级 */
 											 (TaskHandle_t     )&GUI_Task_Handle);/* 任务控制块指针 */
@@ -161,6 +161,7 @@ static void LED_Task(void* parameter)
 {
 	while(1)
 	{
+//    printf("%d\r\n", (int)GUI_ALLOC_GetNumUsedBytes());
 		LED3_TOGGLE;
 		vTaskDelay(1000);
 	}

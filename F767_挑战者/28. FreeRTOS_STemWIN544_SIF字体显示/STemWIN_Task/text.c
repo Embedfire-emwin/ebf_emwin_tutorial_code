970e480818cc81b0634f2216f67fad3fb096e168
hejiawen <hjw0415@utlook.com>
2019-11-19 10:15:52 +0800

    新建sif字体例程，运行一段时间后会花屏卡死

---
+++ F767_挑战者/28. FreeRTOS_STemWIN544_SIF字体显示/STemWIN_Task/text.c
@@ -0,0 +1,4 @@
+﻿const char Framewin_text[] = {"STemWIN@野火电子 STM32F767 "};
+const char text[] = {"欢迎使用野火F767 开发板\r\n本例程使用SIF格式字库，支持中文显示"};
+const char MULTIEDIT_text[] = {"实验平台:野火 STM32F767 开发板\r\n论坛:http://www.firebbs.cn\r\n淘宝:https://fire-stm32.taobao.com"};
+const char *BUTTON_text[] = {"确定","取消"};
